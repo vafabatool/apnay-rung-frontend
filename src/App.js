da3cc4b7cd7d27d48620134ffe675cd1ce5469d2
taimoortt <22100147@lums.edu.pk>
2021-04-08 16:18:38 +0500

    Testing Admin Panel Deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import "./styles.css";
-import AdminSettings from "./AdminSettings";
-import ViewSellers from "./ViewSellers";
-import CustomerNavBar from "./CustomerNavbar";
+// import AdminSettings from "./AdminSettings";
+// import ViewSellers from "./ViewSellers";
+// import CustomerNavBar from "./CustomerNavbar";
 // import SellerNavbar from "./SellerNavbar";
-import Notifications from "./Notifications";
-import QueryForms from "./QueryForms";
-import ViewOrders from "./ViewOrders";
-import ViewAllProducts from "./ViewAllProducts";
+// import Notifications from "./Notifications";
+// import QueryForms from "./QueryForms";
+// import ViewOrders from "./ViewOrders";
+// import ViewAllProducts from "./ViewAllProducts";
 // import Inventory from "./Inventory";
-import ApproveSellers from "./ApproveSellers";
-import Tutorials from "./Tutorials";
+// import ApproveSellers from "./ApproveSellers";
+// import Tutorials from "./Tutorials";
 // import UpdateProduct from "./UpdateProduct";
 // import AddProduct from "./AddProduct";
 // import Product from "./Product";
-import ViewCustomers from "./ViewCustomers";
-import ShoppingCart from "./ShoppingCart";
+// import ViewCustomers from "./ViewCustomers";
+// import ShoppingCart from "./ShoppingCart";
 // import Checkout from "./Checkout";
-import CustomerSettings from "./CustomerSettings";
-import CustomerPanel from "./CustomerPanel";
-import SellerPanel from "./SellerPanel";
-import AdminPanel from "./AdminPanel";
+// import CustomerSettings from "./CustomerSettings";
+// import CustomerPanel from "./CustomerPanel";
+// import SellerPanel from "./SellerPanel";
+// import AdminPanel from "./AdminPanel";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
@@ -27,34 +27,18 @@ export default function App() {
     <Router>
       <Switch>
         <div className="App">
-          <Route path="/AdminSettings" exact component={AdminSettings} />
-          {/* <AdminSettings /> */}
+          {/* <Route path="/AdminSettings" exact component={AdminSettings} />
           <Route path="/CustomerSettings" exact component={CustomerSettings} />
-          {/* <CustomerSettings /> */}
-          {/* <CustomerSettings /> */}
-          {/* <ShoppingCart /> */}
-          {/* <Notifications /> */}
-          {/* <QueryForms /> */}
-          {/* <Inventory /> */}
-          {/* <ApproveSellers /> */}
           <Route path="/ApproveSellers" exact component={ApproveSellers} />
-          {/* <Tutorials /> */}
           <Route path="/Tutorials" exact component={Tutorials} />
           <Route path="/Notifications" exact component={Notifications} />
-          {/* <ViewOrders />  */}
-          {/* <CustomerNavBar /> */}
-          {/* <AdminSettings /> */}
-          {/* <UpdateProduct /> */}
           <Route path="/ViewAllProducts" exact component={ViewAllProducts} />
           <Route path="/ViewOrders" exact component={ViewOrders} />
           <Route path="/ViewSellers" exact component={ViewSellers} />
-          <Route path="/ViewCustomers" exact component={ViewCustomers} />
-          {/* <Product /> */}
-          {/* <AddProduct /> */}
-          {/* <Checkout /> */}
+          <Route path="/ViewCustomers" exact component={ViewCustomers} /> */}
           <Route path="/AdminPanel" exact component={AdminPanel} />
-          <Route exact path="/" component={SellerPanel} />
-          <Route path="/CustomerPanel" exact component={CustomerPanel} />
+          {/* <Route path="/" exact component={SellerPanel} /> */}
+          {/* <Route path="/CustomerPanel" exact component={CustomerPanel} /> */}
         </div>
       </Switch>
     </Router>
